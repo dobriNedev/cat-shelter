ffc50bc8ab1b403f15f9933ba8b928abc784844c
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-11 13:33:25 +0100

    updated the file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const http = require('http');
 
+const cats = require('./cats.json');
+
+const catShelter = require('./views/shelterCat');
+
+const addCat = require('./views/addCat');
+
 const homePage = require('./views/index.html.js');
 
 const siteCss = require('./css/site.css.js');
@@ -9,22 +15,26 @@ const server = http.createServer((req, res) => {
         'Content-Type': 'text/html'
     });
 
-    switch (req.url) {
-        case '/':
-            res.write(homePage);
-            break;
-    
-        case '/css/site.css':
-            res.writeHead(200, {
-                'Content-Type': 'text/css'
-            });
-            res.write(siteCss);
-            break;
-        default:
-            res.write(`
-            <h1>404 Not found!</hi>
-            `);
-            break;
+    if (req.url == '/') {
+        res.write(homePage);
+    } else if (/cats\/\d+\/shelterCat/.test(req.url)) {
+        let catId = req.url.split('/')[2];
+        let cat = cats.find( c => c.id == catId);
+        res.write(catShelter(cat));
+        
+    } else if (req.url == '/css/site.css'){
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        });
+        res.write(siteCss);
+        
+    } else if (req.url == '/cats/add-cat') {
+        res.write(addCat);
+    }
+    else {
+        res.write(`
+        <h1>404 Not found!</hi>
+        `);
     }
     res.end();
 });
